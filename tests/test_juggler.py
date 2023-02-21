9967ef9f0b7026472c9041b7bc0143cdb21f48f5
chinmay <chinmayshah3899@gmail.com>
2023-02-22 01:56:21 +0530

    added black

--- tests/test_juggler.py
+++ tests/test_juggler.py
@@ -2,19 +2,20 @@ from juggler import juggler
 from typing import List
 
 
-
 def square(x):
     return x * x
 
+
 def square_delayed(x):
     import time
+
     time.sleep(1)
     return x * x
 
 
 def test_square():
     len_input = 10
-    input = [ (2, ) for _ in range(len_input) ]
+    input = [(2,) for _ in range(len_input)]
     response: List = juggler.run_process(square, input)
     assert len(response) == len_input
     assert response[0] == 4
@@ -22,7 +23,7 @@ def test_square():
 
 def test_square_delayed():
     len_input = 10
-    input = [ (2, ) for _ in range(len_input) ]
+    input = [(2,) for _ in range(len_input)]
     response: List = juggler.run_process(square_delayed, input)
     assert len(response) == len_input
     assert response[0] == 4
